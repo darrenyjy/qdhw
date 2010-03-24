3843c09ebfd0bf225a0fc21916502cbc5323a783
imcoddy <imcoddy@2a6e4423-fb7f-ddff-0bb9-4b9f62044ba7>
2010-03-24 09:07:32 +0000

    Java Exercise Homework

---
+++ trunk/AdvanceJava/src/hw1/ex3/Car007.java
@@ -0,0 +1,28 @@
+package hw1.ex3;
+
+public class Car007 implements Common
+{
+	int a, b, c;
+
+	public void setA(int a)
+	{
+		this.a = a;
+	}
+
+	public void setB(int b)
+	{
+		this.b = b;
+	}
+
+	public void setC(int c)
+	{
+		this.c = c;
+	}
+
+	public int computeTime(int a, int b, int c)
+	{
+		// TODO Auto-generated method stub
+		return a * b / c;
+	}
+
+}
