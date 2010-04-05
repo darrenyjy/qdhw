7c1af979cf9c34d1c8804c38b14d6338833710c7
imcoddy <imcoddy@2a6e4423-fb7f-ddff-0bb9-4b9f62044ba7>
2010-04-05 02:12:55 +0000

    change layout

--- baseline/src/kwic/es/Output.java
+++ baseline/src/kwic/es/Output.java
@@ -21,64 +21,69 @@
  *  Description:
  *    Output prints sorted lines in a nice format
  * </file>
-*/
+ */
 
 package kwic.es;
 
 /*
  * $Log$
-*/
-
-/**
- *  An instance of the Output class prints sorted lines in nice format.
- *  @author  dhelic
- *  @version $Id$
-*/
-
-public class Output{
-
-//----------------------------------------------------------------------
-/**
- * Fields
- *
- */
-//----------------------------------------------------------------------
-
-//----------------------------------------------------------------------
-/**
- * Constructors
- *
- */
-//----------------------------------------------------------------------
-
-//----------------------------------------------------------------------
-/**
- * Methods
- *
- */
-//----------------------------------------------------------------------
-
-//----------------------------------------------------------------------
-/**
- * Prints the lines at the standard output.
- * @param shift_storage sorted shifts
- * @return void
  */
 
-  public void print(LineStorageWrapper shift_storage){
-    
-        // iterate through all lines
-    for(int i = 0; i < shift_storage.getLineCount(); i++)
-      
-          // print current line
-      System.out.println(shift_storage.getLineAsString(i));
-  }
-
-//----------------------------------------------------------------------
 /**
- * Inner classes
- *
+ * An instance of the Output class prints sorted lines in nice format.
+ * 
+ * @author dhelic
+ * @version $Id$
  */
-//----------------------------------------------------------------------
+
+public class Output
+{
+
+	// ----------------------------------------------------------------------
+	/**
+	 * Fields
+	 * 
+	 */
+	// ----------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------
+	/**
+	 * Constructors
+	 * 
+	 */
+	// ----------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------
+	/**
+	 * Methods
+	 * 
+	 */
+	// ----------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------
+	/**
+	 * Prints the lines at the standard output.
+	 * 
+	 * @param shift_storage
+	 *            sorted shifts
+	 * @return void
+	 */
+
+	public void print(LineStorageWrapper shift_storage)
+	{
+
+		// iterate through all lines
+		for (int i = 0; i < shift_storage.getLineCount(); i++)
+
+			// print current line
+			System.out.println(shift_storage.getLineAsString(i));
+	}
+
+	// ----------------------------------------------------------------------
+	/**
+	 * Inner classes
+	 * 
+	 */
+	// ----------------------------------------------------------------------
 
 }
