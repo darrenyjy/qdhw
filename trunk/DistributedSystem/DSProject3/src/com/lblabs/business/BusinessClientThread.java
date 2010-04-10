1860b97782a405e124e3db0ecb2d1ebddc1c598a
imcoddy <imcoddy@2a6e4423-fb7f-ddff-0bb9-4b9f62044ba7>
2010-04-10 08:53:32 +0000

    Original

---
+++ trunk/DistributedSystem/DSProject3/src/com/lblabs/business/BusinessClientThread.java
@@ -0,0 +1,104 @@
+package com.lblabs.business;
+
+import java.util.Hashtable;
+
+import com.lblabs.net.BookRetailer;
+
+class BusinessClientThread extends Thread
+{
+	String threadID;
+	String workSequence;
+	BookRetailer bookRetailer = new BookRetailer();
+	public BusinessClientThread(String threadID, String workSequence)
+	{
+		this.threadID = threadID;
+		this.workSequence = workSequence;
+	}
+
+	public void run()
+	{
+		int index = workSequence.indexOf("#");
+		String sequenceString;
+		int sequence;
+		while (index >= 0)
+		{
+			sequenceString = workSequence.substring(0, index);
+			workSequence = workSequence.substring(index + 1);
+			Integer bigInt = new Integer(sequenceString);
+			sequence = bigInt.intValue();
+//System.out.println(threadID + ": sequence = " + sequence);
+
+			switch (sequence)
+			{
+				case 0:
+					System.out.println(threadID + ": isConnectionAvailable()");
+					boolean isConnectionAvailable = false;
+					while (!isConnectionAvailable)
+					{
+						isConnectionAvailable = bookRetailer.isConnectionAvailable(threadID);
+System.out.println(threadID + ": isConnectionAvailable = " + isConnectionAvailable);
+						try
+						{
+							Thread.sleep(2000);
+						}
+						catch (Exception e)
+						{
+							System.out.println(e);
+						}
+					}
+					break;
+
+				case 1:
+					System.out.println(threadID + ": signIn()");
+					boolean isSignedIn = bookRetailer.signIn(threadID);
+					if (isSignedIn)
+					{
+						System.out.println(threadID + ": Signin succcessfully!");
+					}
+					else
+					{
+						System.out.println(threadID + ": Signin failed!");
+					}
+					break;
+
+				case 2:
+					System.out.println(threadID + ": signOut()");
+					boolean isSignedOut = bookRetailer.signOut(threadID);
+					if (isSignedOut)
+					{
+						System.out.println(threadID + ": Signout successfully!");
+					}
+					else
+					{
+						System.out.println(threadID + ": Signout failed!");
+					}
+					break;
+
+				case 3:
+					System.out.println(threadID + ": getPrice()");
+					float price = bookRetailer.getPrice("C Language");
+					System.out.println(threadID + ": price = " + price);
+					break;
+
+				case 4:
+					System.out.println(threadID + ": getPublisher()");
+					String publisher = bookRetailer.getPublisher("C Language");
+					System.out.println(threadID + ": publisher = " + publisher);
+					break;
+
+				case 5:
+					System.out.println(threadID + ": getAuthor()");
+					String author = bookRetailer.getAuthor("C Language");
+					System.out.println(threadID + ": author = " + author);
+					break;
+
+				case 6:
+					System.out.println(threadID + ": buyBooks()");
+					Hashtable responseHash = bookRetailer.buyBooks("libing", "111111", "1234567890", "C Language", 2);
+					System.out.println(threadID + ": response = " + responseHash);
+					break;
+			}
+			index = workSequence.indexOf("#");
+		}
+	}
+}
---
+++ trunk/DistributedSystem/DSProject3/src/com/lblabs/net/BookRetailer.java
@@ -0,0 +1,146 @@
+package com.lblabs.net;
+
+import java.io.*;
+import java.net.*;
+import java.util.Hashtable;
+
+public class BookRetailer
+{
+	Socket socket;
+	String host = "127.0.0.1";
+	int port = 8888;
+	int size = 1024;
+	byte[] buffer = new byte[size];
+	String threadID;
+	String request;
+	String response;
+
+	public BookRetailer()
+	{
+		try
+		{
+			socket = new Socket(host, port , false);
+		}
+		catch (IOException e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	public boolean isConnectionAvailable(String threadID)
+	{
+		this.threadID = threadID;
+		request = threadID + "|isConnectionAvailable|" + threadID + "|";
+		response = this.sendRequest(request);
+		return this.convertStringToBoolean(response);
+	}
+
+	public boolean signIn(String threadID)
+	{
+		this.threadID = threadID;
+		request = threadID + "|signIn|" + threadID + "|";
+		response = this.sendRequest(request);
+		return this.convertStringToBoolean(response);
+	}
+
+	public boolean signOut(String threadID)
+	{
+		request = threadID + "|signOut|" + threadID + "|";
+		response = this.sendRequest(request);
+		return this.convertStringToBoolean(response);
+	}
+
+	public float getPrice(String bookName)
+	{
+		request = threadID + "|getPrice|" + bookName + "|";
+		response = this.sendRequest(request);
+		if (response != null)
+		{
+			return this.convertStringToFloat(response);
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+	public String getPublisher(String bookName)
+	{
+		request = threadID + "|getPublisher|" + bookName + "|";
+		response = this.sendRequest(request);
+		return response;
+	}
+
+	public String getAuthor(String bookName)
+	{
+		request = threadID + "|getAuthor|" + bookName + "|";
+		response = this.sendRequest(request);
+		return response;
+	}
+
+	public Hashtable buyBooks(String inputAccountName, String inputAccountPassword, String inputCreditCardNumber, String inputBookName, int inputQuantity)
+	{
+		request = threadID + "|buyBooks|" + inputAccountName + "#" + inputAccountPassword + "#" + inputCreditCardNumber + "#" + inputBookName + "#" + this.convertIntToString(inputQuantity) + "#|";
+		response = this.sendRequest(request);
+		Hashtable responseHash = this.convertStringToHash(response);
+		return responseHash;
+	}
+
+	private String sendRequest(String request)
+	{
+		try
+		{
+			InputStream in = socket.getInputStream();
+			OutputStream out = socket.getOutputStream();
+
+			buffer = request.getBytes();
+
+			out.write(buffer, 0, request.length());
+			out.flush();
+
+			buffer = new byte[size];
+
+			int length = in.read(buffer);
+			response = new String(buffer);
+			int index = response.lastIndexOf("#");
+			return response.substring(0, index);
+		}
+		catch (IOException e)
+		{
+			return null;
+		}
+	}
+
+	private Hashtable convertStringToHash(String strValue)
+	{
+		int num = 0;
+		int index = strValue.indexOf("#");
+		Hashtable hashValue = new Hashtable();
+		String str = null;
+		while (index >= 0)
+		{
+			hashValue.put(str.valueOf(num ++), strValue.substring(0, index));
+			strValue = strValue.substring(index + 1);
+			index = strValue.indexOf("#");
+		}
+		return hashValue;
+	}
+
+	private String convertIntToString(int intValue)
+	{
+		String str = null;
+		return str.valueOf(intValue);
+	}
+
+	private float convertStringToFloat(String floatString)
+	{
+		Float bigFloat = new Float(floatString);
+		return bigFloat.floatValue();
+	}
+
+	private boolean convertStringToBoolean(String boolString)
+	{
+		Boolean bigBool = new Boolean(boolString);
+		return bigBool.booleanValue();
+	}
+}
