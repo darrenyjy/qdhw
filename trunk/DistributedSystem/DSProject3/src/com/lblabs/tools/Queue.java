058d7b2eaba5925efaf54afe219af1f78bbcf116
imcoddy <imcoddy@2a6e4423-fb7f-ddff-0bb9-4b9f62044ba7>
2010-04-15 01:06:00 +0000

    format and refactor str to String

--- trunk/DistributedSystem/DSProject3/src/com/lblabs/tools/Queue.java
+++ trunk/DistributedSystem/DSProject3/src/com/lblabs/tools/Queue.java
@@ -1,17 +1,16 @@
 package com.lblabs.tools;
 
 /*************************************************************************
- *  Compilation:  javac Queue.java
- *  Execution:    java Queue
- *
- *  A queue, implemented using a linked list.
- *
+ * Compilation: javac Queue.java Execution: java Queue
+ * 
+ * A queue, implemented using a linked list.
+ * 
  *************************************************************************/
 
 public class Queue
 {
-	private Node first;        // beginning of queue
-	private Node last;         // end of queue
+	private Node first; // beginning of queue
+	private Node last; // end of queue
 	private int cnt = 0;
 
 	// helper linked list class
@@ -22,24 +21,35 @@ public class Queue
 	}
 
 	// is the queue empty?
-	public boolean isEmpty() { return (first == null); }
+	public boolean isEmpty()
+	{
+		return (first == null);
+	}
 
 	// add anItem to the queue
 	public void enqueue(Object anItem)
 	{
-		cnt ++;
+		cnt++;
 		Node x = new Node();
 		x.item = anItem;
 		x.next = null;
-		if (isEmpty()) { first = x; last = x; }
-		else { last.next = x; last = x; }
+		if (isEmpty())
+		{
+			first = x;
+			last = x;
+		} else
+		{
+			last.next = x;
+			last = x;
+		}
 	}
 
 	// remove and return the least recently added item
 	public Object dequeue()
 	{
-		cnt --;
-		if (isEmpty()) throw new RuntimeException("Queue underflow");
+		cnt--;
+		if (isEmpty())
+			throw new RuntimeException("Queue underflow");
 		Object val = first.item;
 		first = first.next;
 		return val;
@@ -47,9 +57,10 @@ public class Queue
 
 	public Object peek()
 	{
-		if (isEmpty()) throw new RuntimeException("Queue underflow");
+		if (isEmpty())
+			throw new RuntimeException("Queue underflow");
 		Object val = first.item;
-//		first = first.next;
+		// first = first.next;
 		return val;
 	}
 
@@ -60,19 +71,10 @@ public class Queue
 
 	// a test client
 	/*
-	public static void main(String[] args)
-	{
-		Queue q = new Queue();
-		q.enqueue("Vertigo");
-		q.enqueue("Just Lose It");
-		q.enqueue("Pieces of Me");
-		q.enqueue("Pieces of Me");
-		System.out.println(q.dequeue());
-		q.enqueue("Drop It Like It's Hot");
-		while (!q.isEmpty())
-		{
-			System.out.println(q.dequeue());
-		}
-	}
-	*/
+	 * public static void main(String[] args) { Queue q = new Queue();
+	 * q.enqueue("Vertigo"); q.enqueue("Just Lose It");
+	 * q.enqueue("Pieces of Me"); q.enqueue("Pieces of Me");
+	 * System.out.println(q.dequeue()); q.enqueue("Drop It Like It's Hot");
+	 * while (!q.isEmpty()) { System.out.println(q.dequeue()); } }
+	 */
 }
