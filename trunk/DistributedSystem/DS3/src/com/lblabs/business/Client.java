2b921f80795827144f8a3a7d018274430c8282d2
imcoddy <imcoddy@2a6e4423-fb7f-ddff-0bb9-4b9f62044ba7>
2010-06-09 02:53:55 +0000

    change to 10 thread

--- trunk/DistributedSystem/DS3/src/com/lblabs/business/Client.java
+++ trunk/DistributedSystem/DS3/src/com/lblabs/business/Client.java
@@ -1,17 +1,14 @@
 package com.lblabs.business;
 
-import java.io.*;
-import java.util.*;
-
-import com.lblabs.net.BookRetailer;
 
 public class Client
 {
 	public static void main(String args[])
 	{
-		for (int i = 0; i < 15; i ++)
+		for (int i = 0; i < 10; i++)
 		{
-			BusinessClientThread businessClientThread = new BusinessClientThread("T" + i, "0#1#3#4#5#6#2#");
+			BusinessClientThread businessClientThread = new BusinessClientThread(
+					"T" + i, "0#1#3#4#5#6#2#");
 			businessClientThread.start();
 		}
 	}
